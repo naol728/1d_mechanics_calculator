58f408fd8383e60d69ca27f0ff3b2ca8a156bc63
naol <naolmeseret22@gmail.com>
2024-10-26 18:17:23 +0300

    done with the calculator

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,35 +1,71 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {findvelocity,findivelocity,findacceleration,finddisplacment,findtime} from './store'
+import {useDispatch, useSelector} from 'react-redux'
 export default function Calculator() {
+  const [find,setFind]=useState("velocity")
+  const [velocity,setVelocity]=useState("")
+  const [ivelocity,setIvelocity]=useState("")
+  const [displacement,setDisplacment]=useState("")
+  const [acceleration,setAccleration]=useState("")
+  const [time,setTime]=useState("")
+
+  const dispatch=useDispatch()
+  const answer=useSelector((state)=>state.formula.value.answer)
+  const formula=useSelector((state)=>state.formula.value.formula)
+  function handlsubmit(e){
+    e.preventDefault();
+    const values={find,velocity,ivelocity,displacement,acceleration,time}
+    if(values.find == "ivelocity"){
+      dispatch(findivelocity(values))
+    }
+    else if(values.find == "velocity"){
+      dispatch(findvelocity(values))
+    }
+    else if(values.find == "displacemnet"){
+      dispatch(finddisplacment(values))
+    }
+    else if(values.find =="acceleration"){
+      dispatch(findacceleration(values))
+    }
+    else if(values.find =="time"){
+      dispatch(findtime(values))
+    }
+
+  }
+
   return (
     <div  >
       <h1 className='text-3xl font-extrabold text-center italic'> Motion in one dymention calculator</h1>
-
+   
       <div className='flex justify-around items-center flex-wrap h-[80vh]'>
+        <form onSubmit={handlsubmit} >
         <div className='text-xl'>
-            <div className='flex justify-between m-3 font-bold'><p>velocity</p> <input placeholder='m/s'  className='p-1 w-[50%] text-xl ml-1 rounded-md bg-slate-200'/> </div>
-            <div className='flex justify-between m-3 font-bold'> <p>inital v </p><input placeholder='m/s'  className='p-1 w-[50%] text-xl ml-1 rounded-md bg-slate-200'/> </div>
-            <div className='flex justify-between m-3 font-bold' ><p>displacemnt </p><input placeholder='m' className='p-1 w-[50%] text-xl ml-1 rounded-md bg-slate-200'/> </div>
-            <div className='flex justify-between m-3 font-bold'><p>acceleration</p> <input placeholder='m/s^2' className='p-1 w-[50%] text-xl ml-1 rounded-md bg-slate-200'/> </div>
-            <div className='flex justify-between m-3 font-bold'><p>time</p> <input placeholder='t' className='p-1 w-[50%]  text-xl ml-1 rounded-md bg-slate-200'/> </div>
+            <div className='flex justify-between m-3 font-bold'><p>velocity</p> <input value={velocity} onChange={(e)=>setVelocity(Number(e.target.value))} placeholder='m/s'  className='px-1 w-[50%] text-xl ml-1 rounded-md bg-slate-200'/> </div>
+            <div className='flex justify-between m-3 font-bold'> <p>inital v </p><input value={ivelocity} onChange={(e)=>setIvelocity(Number(e.target.value))}  placeholder='m/s'  className='px-1 w-[50%] text-xl ml-1 rounded-md bg-slate-200'/> </div>
+            <div className='flex justify-between m-3 font-bold' ><p>displacemnt </p><input value={displacement} onChange={(e)=>setDisplacment(Number(e.target.value))} placeholder='m' className='px-1 w-[50%] text-xl ml-1 rounded-md bg-slate-200'/> </div>
+            <div className='flex justify-between m-3 font-bold'><p>acceleration</p> <input value={acceleration} onChange={(e)=>setAccleration(Number(e.target.value))} placeholder='m/s^2' className='px-1 w-[50%] text-xl ml-1 rounded-md bg-slate-200'/> </div>
+            <div className='flex justify-between m-3 font-bold'><p>time</p> <input value={time} onChange={(e)=>setTime(Number(e.target.value))} placeholder='t' className='px-1 w-[50%]  text-xl ml-1 rounded-md bg-slate-200'/> </div>
         </div>
         <div className='flex flex-col h-3/6 justify-around items-center' >
             <div>
-              <div className='text-xl font-bold mb-2 '>Find </div>
-              <select value="" className='rounded-md '>
+              <div className='text-xl font-bold mb-2 '>Find</div>
+              <select  value={find} onChange={(e)=>setFind(e.target.value)} className='rounded-md '>
                 <option name="velocity" id="">velocity</option>
-                <option name="inital velocity" id="">inital velocity</option>
+                <option name="ivelocity" id="">ivelocity</option>
                 <option name="displacemnet" id="">displacemnet</option>
                 <option name="acceleration" id="">acceleration</option>
                 <option name="time" id="">time</option>
               </select>
+              <button className='text-lg font-semibold ml-2 bg-blue-200 px-3 rounded-md '>calculate</button>
             </div>
 
-            <div className='flex flex-col'>
-              <div className='mb-2 font-semibold text-lg'>formula=v=u+at</div>
-              <div className='mt-2 font-extrabold text-xl'>answer=2.3m/s</div>
-            </div>
-        </div>
+   
+            {formula && answer ? <div className='flex flex-col'>
+              <div className='mb-2 mt-2 font-semibold text-lg'>formula: {formula}</div>
+              <div className='mt-2 font-extrabold text-xl'>answer={answer}</div>
+            </div>:<></>}
+           </div>
+        </form>
       </div>
     </div>
   )
--- src/store.jsx
+++ src/store.jsx
@@ -1,17 +1,96 @@
 import {createSlice,configureStore} from "@reduxjs/toolkit"
 const formulaslice=createSlice({
     name:"formula",
-    initialState:{value:{name:""}},
+    initialState:{value:{find:"",velocity:0,ivelocity:0,acceleration:0,displacement:0,time:0,formula:"",answer:0}},
     reducers:{
-        login:(state,action)=>{
-            state.value=action.payload
-        },
-        logout:(state)=>{
-            state,value=initialState.value
+      findvelocity:(state,actions)=>{
+        state.value=actions.payload
+        if(state.value.displacement==0 || state.value.displacement==""){
+           state.value.answer=state.value.ivelocity+(state.value.acceleration*state.value.time)
+           state.value.formula="v=u+at";
         }
+
+        else if (state.value.acceleration==0 || state.value.acceleration==""){
+         state.value.answer=state.value.displacement-state.value.time-state.value.ivelocity
+         state.value.formula="v=s-t-u"
+        }
+        else if(state.value.time==0 || state.value.time==""){
+         state.value.answer=Math.sqrt((Math.pow(state.value.ivelocity,2))+(2*(state.value.acceleration*state.value.displacement)))
+        state.value.formula="v^2=u^2-2as"
+        }
+      },
+      findivelocity:(state,actions)=>{
+        state.value=actions.payload
+         if(state.value.displacement==0 || state.value.displacement==""){
+            state.value.answer=state.value.velocity-(state.value.acceleration*state.value.time)
+            state.value.formula="u=v-at";
+         }
+
+         else if (state.value.velocity==0 || state.value.velocity==""){
+          state.value.answer=(state.value.displacement/state.value.time)-((state.value.acceleration*state.value.time)/2)
+          state.value.formula="u=s/t-at/2"
+         }
+         else if(state.value.time==0 || state.value.time==""){
+          state.value.answer=Math.sqrt((Math.pow(state.value.velocity,2))-(2*(state.value.acceleration*state.value.displacement)))
+         state.value.formula="u^2=v^2-2as"
+         }
+         else if(state.value.acceleration==0 || state.value.acceleration==""){
+          state.value.answer=state.value.displacement-state.value.time-state.value.velocity;
+          state.value.formula="u=s-t-u";
+         }
+
+      },
+      findacceleration:(state,actions)=>{
+        state.value=actions.payload
+        if(state.value.displacement==0 || state.value.displacement==""){
+           state.value.answer=(state.value.ivelocity+state.value.velocity)/state.value.time
+           state.value.formula="a=(v-u)/2";
+        }
+
+        else if (state.value.velocity==0 || state.value.velocity==""){
+         state.value.answer=(state.value.displacement-state.value.ivelocity)*(2/Math.pow(state.value.time,2))
+         state.value.formula="a=(s-u)*2/t^2"
+        }
+        else if(state.value.time==0 || state.value.time==""){
+         state.value.answer=(Math.pow(state.value.ivelocity,2))+Math.pow(state.value.velocity,2)/(2*state.value.displacement)
+        state.value.formula="a=(v^2-u^2)/2s"
+        }
+      },
+      finddisplacment:(state,actions)=>{
+        state.value=actions.payload
+        if(state.value.acceleration==0 || state.value.acceleration==""){
+           state.value.answer=((state.value.ivelocity+state.value.velocity)/2)+(state.value.time)
+           state.value.formula="s=((u+v)/2)+t";
+        }
+
+        else if (state.value.velocity==0 || state.value.velocity==""){
+         state.value.answer=state.value.ivelocity+(0.5*state.value.acceleration*Math.pow(state.value.time,2))
+         state.value.formula="s=u+1/2*at^2"
+        }
+        else if(state.value.time==0 || state.value.time==""){
+         state.value.answer=(Math.pow(state.value.velocity,2)-Math.pow(state.value.ivelocity,2))/(2*state.value.acceleration)
+        state.value.formula="s=v^2+u^2/2a"
+        }
+      },
+      findtime:(state,actions)=>{
+        state.value=actions.payload
+        if(state.value.acceleration==0 || state.value.acceleration==""){
+           state.value.answer=state.value.time-((state.value.ivelocity+state.value.velocity)/2)
+           state.value.formula="t=s-((u+v)/2)";
+        }
+
+        else if (state.value.velocity==0 || state.value.velocity==""){
+         state.value.answer=Math.sqrt((state.value.displacement-state.value.ivelocity)*(2/state.value.acceleration))
+         state.value.formula="t=(s-u)2/a"
+        }
+        else if(state.value.displacement==0 || state.value.displacement==""){
+         state.value.answer=(state.value.velocity+state.value.ivelocity)/state.value.acceleration
+        state.value.formula="t=(v-u)/a"
+        }
+      },
     }
 })
-export const {login,logout} =formulaslice.actions;
+export const {findvelocity,findivelocity,findacceleration,finddisplacment,findtime} =formulaslice.actions;
 export const store=configureStore({
     reducer:{
         formula:formulaslice.reducer,
